fb8677fb4d4faa623ee62eed5da0b60792607e6f
jorgepz <jorgepz@fing.edu.uy>
2021-06-22 16:08:15 -0300

    .

--- Example/src/Example.jl
+++ Example/src/Example.jl
@@ -6,9 +6,21 @@ export func
     func(x)
 
 Returns double the number `x` plus `1`.
+
+Test including figure
+
+Here's an equation:
+
+``\\frac{n!}{k!(n - k)!} = \\binom{n}{k}``
+
+
+```@raw html
+<img src="Example/drawing.svg" alt="example drawing" width="500"/>
+```
+
+
 """
 func(x) = 2x + 1
 
 
 end
-
